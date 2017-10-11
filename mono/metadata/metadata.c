zioshin/Unity-mono

commit 92986297f810db135b0fb97553038fb649ae504d

Only force 4 byte for double/int64_t on iOS cross compiles (case 898636)

mono/metadata/metadata.c
@@ -3893,15 +3893,15 @@ mono_type_size (MonoType *t, int *align)
 		return 4;
 	case MONO_TYPE_I8:
 	case MONO_TYPE_U8:
-#if defined(TARGET_ARM)                                                                                                                                                                                                                                               
-                *align = 4;                                                                                                                                                                                                                                            
+#if defined(PLATFORM_IPHONE_XCOMP)
+		*align = 4;
 #else
 		*align = __alignof__(gint64);
 #endif
 		return 8;		
 	case MONO_TYPE_R8:
-#if defined(TARGET_ARM)                                                                                                                                                                                                                                               
-                *align = 4;                                                                                                                                                                                                                                            
+#if defined(PLATFORM_IPHONE_XCOMP)
+		*align = 4;
 #else
 		*align = __alignof__(double);
 #endif

leesGb/mono

commit 0421b2a1a166fb25bb7afdc9637165d7962ba20e

Fix 64-bit type packing in the iOS runtime (case 949127)
This regression has been caused by 92986297f810. The function in
question is used by both the runtime and the cross-compiler, so both
must use the same alignment settings.

mono/metadata/metadata.c
@@ -3893,14 +3893,14 @@ mono_type_size (MonoType *t, int *align)
 		return 4;
 	case MONO_TYPE_I8:
 	case MONO_TYPE_U8:
-#if defined(PLATFORM_IPHONE_XCOMP)
+#if defined(PLATFORM_IPHONE) || defined(PLATFORM_IPHONE_XCOMP)
 		*align = 4;
 #else
 		*align = __alignof__(gint64);
 #endif
 		return 8;		
 	case MONO_TYPE_R8:
-#if defined(PLATFORM_IPHONE_XCOMP)
+#if defined(PLATFORM_IPHONE) || defined(PLATFORM_IPHONE_XCOMP)
 		*align = 4;
 #else
 		*align = __alignof__(double);